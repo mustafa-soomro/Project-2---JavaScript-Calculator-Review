41c15efe3091f9155ba2d4657921e92f8eedfb5b
mustafa-soomro <mustafagulsoomro@gmail.com>
2023-10-05 11:48:06 -0400

    Added operator functionality Finished all function

--- script.js
+++ script.js
@@ -36,12 +36,55 @@ document.getElementById('entry').value = newVal
 
 
 function  mathButPress(operator){
-    
-}
+    //Check if there was a previous calculation by seeing if resultVal has a value. If result value doesn't have a value then stor the current val as a previous for the next calculation
+    if(!resultVal){
+        prevVal = newVal
+    }
+    else {
+    // If there is a current result store that as the previous value entered 
+        prevVal = resultVal
+    }
+    // Restart creation of new number
+    newVal = ''
+    // Reset decimalClicked
+    decimalClicked = false
+    // Stores operator clicked
+    mathOperator = operator
+    // Prepare entry for receiving new numbers
+    resultVal = ''
+    document.getElementById('entry').value = ''
+}  
 
 
 function  equalButPress(){
-    
+    // Reset decimalClicked
+    decimalClicked = false
+    //Converts string numbers to floats
+    prevVal = parseFloat(prevVal)
+    newVal = parseFloat(newVal)
+    //Perform calculations based on stored operator
+    switch (mathOperator){
+        case '+':
+            resultVal = prevVal + newVal
+            break
+        case '-':
+            resultVal = prevVal - newVal
+            break
+        case '*':
+            resultVal = prevVal * newVal
+            break
+        case '/':
+            resultVal = prevVal / newVal
+            break
+        // If equals is hit without an operator, leave everything as is 
+        default: 
+            resultVal = newVal
+    }
+    // Store the current value pn the previous so that I can except to next value in the calculation
+    prevVal = newVal
+    // Put the calculation result in the entry window
+    document.getElementById('entry').value = resultVal
+
 }
 
 // Clears everything except set memory 
